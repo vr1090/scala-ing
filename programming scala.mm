f14ed608c3f05296434fff097fecb71116e626d7
inu <vr1090@gmail.com>
2017-04-22 13:35:10 +0700

    add source code example

---
+++ programming scala.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1492842241927" ID="ID_1962551922" MODIFIED="1492842250157" TEXT="programming scala"/>
+</map>
