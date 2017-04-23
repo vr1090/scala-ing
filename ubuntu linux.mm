364e2fa2ec44a8d8f33319900de209c064dea9fd
inu <vr1090@gmail.com>
2017-04-23 21:48:22 +0700

    sekalian ubuntu documentation

---
+++ ubuntu linux.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1492958455956" ID="ID_1098157115" MODIFIED="1492958462993" TEXT="ubuntu linux">
+<node CREATED="1492958463559" ID="ID_412700894" MODIFIED="1492958470843" POSITION="right" TEXT="setidaknya lu master satu OS">
+<node CREATED="1492958471504" ID="ID_355610332" MODIFIED="1492958473881" TEXT="udah itu aja"/>
+<node CREATED="1492958474498" ID="ID_825850959" MODIFIED="1492958478121" TEXT="ga usah muluk2"/>
+</node>
+<node CREATED="1492958480273" ID="ID_763148392" MODIFIED="1492958483770" POSITION="left" TEXT="command line">
+<node CREATED="1492958484063" ID="ID_1481399049" MODIFIED="1492958508578" TEXT="/usr/share/man">
+<node CREATED="1492958491786" ID="ID_104375922" MODIFIED="1492958497706" TEXT="tempat naro man page"/>
+</node>
+<node CREATED="1492958788874" ID="ID_1283198890" MODIFIED="1492958791027" TEXT="apropos">
+<node CREATED="1492958791337" ID="ID_1978274300" MODIFIED="1492958797868" TEXT="buat nyari command buat gimana"/>
+</node>
+</node>
+</node>
+</map>
